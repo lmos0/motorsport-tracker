2a560d9a83a1025927f80e562b49a5a589a2f489
lmos0 <luismarcio.os@outlook.com>
2025-03-05 00:35:56 -0300

    Add route for retrieving driver results by ID

--- src/routes/resultsRoutes.ts
+++ src/routes/resultsRoutes.ts
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/', getResults)
 router.get('/:id', getResultById as RequestHandler)
 router.get("/:driverId/points", getDriverTotalPoints as RequestHandler);
+router.get("/:driverId/results", getDriverResultsById as RequestHandler);
 //router.post('/', addResult as RequestHandler)
 //router.put('/:id', updateResult as RequestHandler)
 //router.delete('/:id', deleteResult as RequestHandler)
